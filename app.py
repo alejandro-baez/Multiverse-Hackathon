68593a54fc416ad75a7567097c10e5bfc3ad2ec2
Alejandro Baez <abaez2623@gmail.com>
2023-09-19 16:04:20 -0400

    Created api spotify token and stored sensitive info into .env file

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ CREATE_ARTIST_TABLE = (
     "CREATE TABLE IF NOT EXISTS artist (id SERIAL PRIMARY KEY, name TEXT);"
 )
 
+CREATE_ALBUM_TABLE = (
+    "CREATE TABLE IF NOT EXISTS album (id SERIAL PRIMARY KEY, title TEXT, genre TEXT, release_date DATE, artist_id INTEGER ,FOREIGN KEY(artist_id) REFERENCES artist(id) ON DELETE CASCADE);"
+)
+
+CREATE_SONG_TABLE = """ CREATE TABLE IF NOT EXISTS song (
+                        id SERIAL PRIMARY KEY
+                    );"""
+
 app = Flask(__name__)
 
 conn = psycopg2.connect(f'dbname=hackathon_db user=postgres password={my_db_pw}')
@@ -24,6 +32,9 @@ if __name__ == "__main__":
     with conn:
         with conn.cursor() as cursor:
             cursor.execute(CREATE_ARTIST_TABLE)
+            cursor.execute(CREATE_ALBUM_TABLE)
+
+
     app.run(debug=True)
 
 
